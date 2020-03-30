7ebae2181d348e8593261ddcc91949245eac25eb
burijillo <burijillo@gmail.com>
2020-03-30 17:02:05 +0200

    Added plot tab

--- COVID_Visualization/COVID_Visualization/MainScreen.Designer.cs
+++ COVID_Visualization/COVID_Visualization/MainScreen.Designer.cs
@@ -35,14 +35,22 @@
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.getDataFromWebButton = new System.Windows.Forms.Button();
             this.mainDataPanel = new System.Windows.Forms.Panel();
-            this.plotsTabPage = new System.Windows.Forms.TabPage();
             this.mainDataGridView = new System.Windows.Forms.DataGridView();
+            this.plotsTabPage = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.refreshPlotButton = new System.Windows.Forms.Button();
+            this.countryComboBox = new System.Windows.Forms.ComboBox();
+            this.plotTypeComboBox = new System.Windows.Forms.ComboBox();
             this.mainTabControl.SuspendLayout();
             this.configTabPage.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.mainDataPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mainDataGridView)).BeginInit();
+            this.plotsTabPage.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainTabControl
@@ -121,16 +129,6 @@
             this.mainDataPanel.Size = new System.Drawing.Size(786, 390);
             this.mainDataPanel.TabIndex = 1;
             // 
-            // plotsTabPage
-            // 
-            this.plotsTabPage.Location = new System.Drawing.Point(4, 22);
-            this.plotsTabPage.Name = "plotsTabPage";
-            this.plotsTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.plotsTabPage.Size = new System.Drawing.Size(792, 424);
-            this.plotsTabPage.TabIndex = 1;
-            this.plotsTabPage.Text = "Plots";
-            this.plotsTabPage.UseVisualStyleBackColor = true;
-            // 
             // mainDataGridView
             // 
             this.mainDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -139,6 +137,87 @@
             this.mainDataGridView.Name = "mainDataGridView";
             this.mainDataGridView.Size = new System.Drawing.Size(786, 390);
             this.mainDataGridView.TabIndex = 0;
+            // 
+            // plotsTabPage
+            // 
+            this.plotsTabPage.Controls.Add(this.tableLayoutPanel3);
+            this.plotsTabPage.Location = new System.Drawing.Point(4, 22);
+            this.plotsTabPage.Name = "plotsTabPage";
+            this.plotsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.plotsTabPage.Size = new System.Drawing.Size(792, 424);
+            this.plotsTabPage.TabIndex = 1;
+            this.plotsTabPage.Text = "Plots";
+            this.plotsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.38931F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.61069F));
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(786, 418);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Controls.Add(this.refreshPlotButton, 0, 9);
+            this.tableLayoutPanel4.Controls.Add(this.countryComboBox, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.plotTypeComboBox, 0, 1);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(623, 0);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 10;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(163, 418);
+            this.tableLayoutPanel4.TabIndex = 0;
+            // 
+            // refreshPlotButton
+            // 
+            this.refreshPlotButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.refreshPlotButton.Location = new System.Drawing.Point(3, 372);
+            this.refreshPlotButton.Name = "refreshPlotButton";
+            this.refreshPlotButton.Size = new System.Drawing.Size(157, 43);
+            this.refreshPlotButton.TabIndex = 0;
+            this.refreshPlotButton.Text = "Refresh";
+            this.refreshPlotButton.UseVisualStyleBackColor = true;
+            this.refreshPlotButton.Click += new System.EventHandler(this.refreshPlotButton_Click);
+            // 
+            // countryComboBox
+            // 
+            this.countryComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.countryComboBox.FormattingEnabled = true;
+            this.countryComboBox.Location = new System.Drawing.Point(3, 3);
+            this.countryComboBox.Name = "countryComboBox";
+            this.countryComboBox.Size = new System.Drawing.Size(157, 21);
+            this.countryComboBox.TabIndex = 1;
+            // 
+            // plotTypeComboBox
+            // 
+            this.plotTypeComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plotTypeComboBox.FormattingEnabled = true;
+            this.plotTypeComboBox.Location = new System.Drawing.Point(3, 44);
+            this.plotTypeComboBox.Name = "plotTypeComboBox";
+            this.plotTypeComboBox.Size = new System.Drawing.Size(157, 21);
+            this.plotTypeComboBox.TabIndex = 2;
             // 
             // MainScreen
             // 
@@ -154,6 +233,9 @@
             this.tableLayoutPanel2.ResumeLayout(false);
             this.mainDataPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.mainDataGridView)).EndInit();
+            this.plotsTabPage.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -168,6 +250,11 @@
         private System.Windows.Forms.Button getDataFromWebButton;
         private System.Windows.Forms.Panel mainDataPanel;
         private System.Windows.Forms.DataGridView mainDataGridView;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button refreshPlotButton;
+        private System.Windows.Forms.ComboBox countryComboBox;
+        private System.Windows.Forms.ComboBox plotTypeComboBox;
     }
 }
 
--- COVID_Visualization/COVID_Visualization/MainScreen.cs
+++ COVID_Visualization/COVID_Visualization/MainScreen.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+using OxyPlot.WindowsForms;
+using OxyPlot.Annotations;
 
 using COVID_Visualization.Forms;
 using COVID_Visualization.Data;
@@ -17,12 +22,17 @@ namespace COVID_Visualization
     public partial class MainScreen : Form
     {
         private bool isDataParsed = false;
-        Dictionary<string, DataNational> globalData_dict = new Dictionary<string, DataNational>();
+        Dictionary<string, DataNational> globalDataConfirmed_dict = new Dictionary<string, DataNational>();
+        Dictionary<string, DataNational> globalDataDeaths_dict = new Dictionary<string, DataNational>();
+        Dictionary<string, DataNational> globalDataRecovered_dict = new Dictionary<string, DataNational>();
+        List<string> countryList = new List<string>();
 
         public event EventHandler _data_parsed_event;
 
         WaitForm waitForm;
         BackgroundWorker getData_bg;
+        PlotView mainPlotView;
+        PlotModel MainPlotModel;
 
         #region Initialization
 
@@ -31,6 +41,7 @@ namespace COVID_Visualization
             InitializeComponent();
 
             InitializeMainScreen();
+            InitializePlotView();
 
             _data_parsed_event += _data_parsed_triggered;
 
@@ -44,6 +55,24 @@ namespace COVID_Visualization
             isDataParsed = false;
 
             mainDataPanel.Enabled = false;
+        }
+
+        private void InitializePlotView()
+        {
+            MainPlotModel = new PlotModel();
+            mainPlotView = new PlotView();
+
+            MainPlotModel.PlotType = PlotType.XY;
+            mainPlotView.Model = MainPlotModel;
+            MainPlotModel.LegendPosition = LegendPosition.LeftTop;
+            MainPlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, MaximumPadding = 0.1, MinimumPadding = 0.1 });
+            MainPlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, MaximumPadding = 0.1, MinimumPadding = 0.1, MajorGridlineStyle = LineStyle.Solid });
+
+            mainPlotView.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Controls.Add(mainPlotView, 0, 0);
+
+            plotTypeComboBox.DataSource = Enum.GetValues(typeof(PlotTypes));
+            plotTypeComboBox.SelectedItem = PlotTypes.Confirmed_deaths_recovered.ToString();
         }
 
         #endregion
@@ -62,7 +91,9 @@ namespace COVID_Visualization
         {
             DataParser dataParser = new DataParser();
 
-            dataParser.CSVParser(@"D:\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_global.csv", out globalData_dict);
+            dataParser.CSVParser(@"D:\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_global.csv", out globalDataConfirmed_dict);
+            dataParser.CSVParser(@"D:\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_deaths_global.csv", out globalDataDeaths_dict);
+            dataParser.CSVParser(@"D:\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_recovered_global.csv", out globalDataRecovered_dict);
             _data_parsed_event?.Invoke(sender, e);
         }
 
@@ -79,16 +110,20 @@ namespace COVID_Visualization
                 // Initialize mainDataGridView
                 mainDataGridView.Invoke((Action)delegate
                 {
-                    mainDataGridView.ColumnCount = 2;
+                    mainDataGridView.ColumnCount = 4;
                     mainDataGridView.Columns[0].Name = "Country";
                     mainDataGridView.Columns[1].Name = "Confirmed cases";
+                    mainDataGridView.Columns[2].Name = "Deaths";
+                    mainDataGridView.Columns[3].Name = "Recovered";
                 });
 
-                foreach(var item in globalData_dict)
+                foreach(var item in globalDataConfirmed_dict)
                 {
                     string country = item.Key;
                     int confirmed = item.Value.NATIONAL_DATA.DATA.Values.ToArray()[item.Value.NATIONAL_DATA.DATA.Count - 1];
-                    string[] row = new string[] { country, confirmed.ToString() };
+                    int deaths = globalDataDeaths_dict[country].NATIONAL_DATA.DATA.Values.ToArray()[item.Value.NATIONAL_DATA.DATA.Count - 1];
+                    int recovered = globalDataRecovered_dict[country].NATIONAL_DATA.DATA.Values.ToArray()[item.Value.NATIONAL_DATA.DATA.Count - 1];
+                    string[] row = new string[] { country, confirmed.ToString(), deaths.ToString(), recovered.ToString() };
 
                     mainDataGridView.Invoke((Action)delegate
                     {
@@ -96,6 +131,65 @@ namespace COVID_Visualization
                     });
                 }
             }
+        }
+
+        private void GetCountryList()
+        {
+            if (isDataParsed)
+            {
+                countryList = globalDataConfirmed_dict.Keys.ToList();
+            }
+        }
+
+        #endregion
+
+        #region Plot tab
+
+        private void PlotDataIntoView(List<PointF> _plotPointsSerie, Color _colorSerie, string _nameSerie)
+        {
+            LineSeries serie = new LineSeries();
+
+            serie.MarkerSize = 2;
+            serie.MarkerFill = Color.Gray.ToOxyColor();
+            serie.MarkerType = MarkerType.Circle;
+            serie.LineStyle = LineStyle.Solid;
+            serie.Color = _colorSerie.ToOxyColor();
+            serie.Title = _nameSerie;
+            //serie.Smooth = true;
+
+            foreach (var item in _plotPointsSerie)
+            {
+                DataPoint point = new DataPoint(item.X, item.Y);
+                serie.Points.Add(point);
+            }
+
+            MainPlotModel.Series.Add(serie);
+            mainPlotView.InvalidatePlot(true);
+        }
+
+        private void refreshPlotButton_Click(object sender, EventArgs e)
+        {
+            if(isDataParsed)
+            {
+                // Clear plot
+                clearPlot();
+
+                List<string> SeriesNames = new List<string>();
+                List<List<PointF>> globalList = GetListOfPointsToPlot((PlotTypes)Enum.Parse(typeof(PlotTypes), plotTypeComboBox.SelectedItem.ToString()), out SeriesNames);
+
+                for (int i = 0; i < globalList.Count; i++)
+                {
+                    PlotDataIntoView(globalList[i], ColorTypes()[i], SeriesNames[i]);
+                }
+            }
+        }
+
+        private void clearPlot()
+        {
+            MainPlotModel.Series.Clear();
+            MainPlotModel.Axes[0].Reset();
+            MainPlotModel.Axes[1].Reset();
+            mainPlotView.InvalidatePlot(true);
         }
 
         #endregion
@@ -122,6 +216,85 @@ namespace COVID_Visualization
             {
                 getDataFromWebButton.Enabled = false;
             });
+
+            GetCountryList();
+
+            // List all countries to plot
+            countryComboBox.Invoke((Action)delegate
+            {
+                foreach(string country in countryList)
+                {
+                    countryComboBox.Items.Add(country);
+                }
+
+                countryComboBox.SelectedItem = "Spain";
+                countryComboBox.Enabled = true;
+            });
         }
+
+        #region Plot types
+
+        private enum PlotTypes : int
+        {
+            Confirmed_deaths_recovered = 0,
+            Confirmed = 1
+        }
+
+        private Color[] ColorTypes()
+        {
+            return new Color[] { Color.Black, Color.DarkBlue, Color.DarkRed, Color.DarkGoldenrod };
+        }
+
+        private List<List<PointF>> GetListOfPointsToPlot(PlotTypes plotType, out List<string> seriesNames)
+        {
+            List<List<PointF>> result = new List<List<PointF>>();
+            seriesNames = new List<string>();
+
+            switch (plotType)
+            {
+                case PlotTypes.Confirmed_deaths_recovered:
+                    // Get confirmed cases
+                    seriesNames.Add("Confirmed");
+                    List<PointF> confirmed_pointList = new List<PointF>();
+                    float counter = 0;
+                    foreach (var item in globalDataConfirmed_dict[countryComboBox.Text].NATIONAL_DATA.DATA)
+                    {
+                        float value = (float)item.Value;
+                        confirmed_pointList.Add(new PointF(counter, value));
+                        counter++;
+                    }
+                    result.Add(confirmed_pointList);
+                    counter = 0;
+
+                    // Get death cases
+                    seriesNames.Add("Deaths");
+                    List<PointF> deaths_pointList = new List<PointF>();
+                    foreach (var item in globalDataDeaths_dict[countryComboBox.Text].NATIONAL_DATA.DATA)
+                    {
+                        float value = (float)item.Value;
+                        deaths_pointList.Add(new PointF(counter, value));
+                        counter++;
+                    }
+                    result.Add(deaths_pointList);
+                    counter = 0;
+
+                    // Get recovered cases
+                    seriesNames.Add("Recovered");
+                    List<PointF> recovered_pointList = new List<PointF>();
+                    foreach (var item in globalDataRecovered_dict[countryComboBox.Text].NATIONAL_DATA.DATA)
+                    {
+                        float value = (float)item.Value;
+                        recovered_pointList.Add(new PointF(counter, value));
+                        counter++;
+                    }
+                    result.Add(recovered_pointList);
+
+                    break;
+            }
+
+            return result;
+        }
+
+        #endregion
     }
 }
